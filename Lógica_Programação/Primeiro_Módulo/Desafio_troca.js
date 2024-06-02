6eb6035aabdfce5d3615e3c72ca5b54b8d4c0e17
Gladston Moraes <gwm.beto@hotmail.com>
2024-06-02 20:13:08 -0300

    asd

---
+++ Lógica_Programação/Primeiro_Módulo/Desafio_troca.js
@@ -0,0 +1,14 @@
+// O desafio consiste em atribuir o valor de a para b e atribuir o valor de b para a
+
+// Para iso ele criou uma terceira variável para guardar o valor de a
+
+
+let a = 7;
+let b = 94;
+let temp = a
+
+a = b;
+b = temp;
+
+console.log (a );
+console.log(b);
---
+++ Lógica_Programação/Segundo_Módulo_Operadores/Desafio_aritimeticos.js
@@ -0,0 +1,9 @@
+//f - 32 / 9 = C / 5
+
+let f = 73;
+let c = (f - 32) / 9
+console.log(c)
+
+let celcius = c*5
+console.log(celcius)
+
