3193deed671c21e6b91c675594efc1386937addf
Gladston Moraes <gwm.beto@hotmail.com>
2024-06-02 14:19:54 -0300

    commit

---
+++ Lógica_Programação/Primeiro_Módulo/Dados_4.js
@@ -0,0 +1,8 @@
+const a = 3;
+
+console.log(a);
+
+//a = 10;
+//console.log(a) 
+
+//não se pode alterar o valor de uma constante
---
+++ Lógica_Programação/Primeiro_Módulo/Desafio_area.js
@@ -0,0 +1,10 @@
+//Fórmula para calcular área de uma circunferência
+
+// PI * raio * raio
+
+const PI = 3.141592
+let raio = 10
+let circunferencia = PI * raio * raio
+let resultado = circunferencia
+
+console.log("A área da circunferência é " + resultado + " M2")
