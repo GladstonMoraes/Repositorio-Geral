dedc18616a3e144734452138c195c3188b03e115
Gladston Moraes <gwm.beto@hotmail.com>
2024-06-04 20:53:35 -0300

    salvando arquivos de aulas

--- Lógica_Programação/Segundo_Módulo_Operadores/Desafio_Atribuicao.js
+++ Lógica_Programação/Segundo_Módulo_Operadores/Desafio_Atribuicao.js
@@ -1,10 +1,9 @@
 let texto = "Lista de Aprovados";
 
-texto += "\n==================";
-texto += "\n \n";
-texto += "1. Ana Silva";
-texto += "\n2. Pedro Albuquerque";
-texto += "\n3. Guilherme Pereira";
-texto += "\n4. Rebeca França";
+texto += "\n==================\n\n";
+texto += "1. Ana Silva\n";
+texto += "2. Pedro Albuquerque\n";
+texto += "3. Guilherme Pereira\n";
+texto += "4. Rebeca França\n";
 
 console.log(texto);
---
+++ Lógica_Programação/Segundo_Módulo_Operadores/Desafio_relacionais.js
@@ -0,0 +1,22 @@
+// Encontre 5 expressões que retorne o valor Falso e 5 que retorne o valor verdadeiro
+
+let a = 10;
+let b = 15;
+
+//falsas
+
+console.log(a == b);
+console.log(a + b == a);
+console.log(a === b);
+console.log(a > b);
+console.log(a >= b);
+
+console.log("\n")
+
+//verdadeiras
+
+console.log(a !== b);
+console.log(b - 5 == a);
+console.log(a < b);
+console.log(b > a);
+console.log(a + 5 == b);
