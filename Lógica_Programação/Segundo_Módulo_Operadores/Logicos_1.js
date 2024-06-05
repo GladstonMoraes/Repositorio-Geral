5339f82ce6d47d2994e9ecd3d2d0bb2736301ab3
Gladston Moraes <gwm.beto@hotmail.com>
2024-06-04 22:43:29 -0300

    aula pratica logicos

--- Lógica_Programação/Segundo_Módulo_Operadores/Logicos_1.js
+++ Lógica_Programação/Segundo_Módulo_Operadores/Logicos_1.js
@@ -16,10 +16,12 @@ console.log(resultadoOu);
 //Exemplo de Xor (ou exclusivo) onde ele compara se os valores são diferentes, podendo ser tbm comparados estritamente 
 //para comparar tbm o tipo de dado, como está no exemplo
 
-console.log(true !== true);
-console.log(true !== false);
-console.log(false !== true);
-console.log(false !== false);
+console.log("Verdadeiro é diferente de verdadeiro? " + (true !== true));
+console.log("Verdadeiro é diferente de falso? " + (true !== false));
+console.log("Falso é diferente de falso? " + (false !== true));
+console.log("Falso é diferente de falso? " + (false !== false));
+
+// a vírgula substitui o operador de concatenação dentro da sentença
 
 //Exemplo de negação Lógica, onde o resultado é simplesmente o contrário da afirmação (Operador unário)
 
