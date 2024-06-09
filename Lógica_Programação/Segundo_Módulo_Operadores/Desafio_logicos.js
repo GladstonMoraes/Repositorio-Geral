d8759a4b008b210ca99c573755ac6c6c2f904e41
Gladston Moraes <gwm.beto@hotmail.com>
2024-06-09 17:51:57 -0300

    commit

---
+++ Lógica_Programação/Segundo_Módulo_Operadores/Desafio_logicos.js
@@ -0,0 +1,13 @@
+//Duas expressões resultando em TRUE (Aritiméticos/Lógicos/Relacionais)
+let verdadeira1 = 2 + 5 >= 7 && 8 > 2;
+let verdadeira2  = !(6 * 6 < 3);
+
+console.log(verdadeira1);
+console.log(verdadeira2);
+
+//Duas expressões resultando em FALSE (Aritiméticos/Lógicos/Relacionais)
+let falso1 = 8 - 4 >12 || false || 15 === "15";
+let falso2 = !(7 !== 6 + 1 || "3" !== 3);
+
+console.log(falso1);
+console.log(falso2);
